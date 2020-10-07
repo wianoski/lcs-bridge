d4d4a109c55932322af45ba56b54a935edb36a0d
wianoski <thirafi.wian@gmail.com>
2020-10-07 15:11:39 +0700

    calib mass

---
+++ RTU/fix_RTU04/calib/src/calb.cpp
@@ -0,0 +1,108 @@
+#include "HX711.h"
+
+// HX711 circuit wiring
+#define SCK_OUT A2
+#define DOUT A1
+
+HX711 scale;
+
+//kita punya persamaan y = ax+b
+//dimana y adalah reading dari hx711
+//dan x adalah beban yang diterima
+
+float x0 = 0,
+      y0 = 0,
+      a = 0,
+      b = 0,
+      res = 0;
+
+long reading_Start = 0,
+     current_Reading = 0;
+
+const byte numChars = 32;
+char receivedChars[numChars];
+char tempChars[numChars];        // temporary array for use when parsing
+
+      // variables to hold the parsed data
+char messageFromPC[numChars] = {0};
+int integerFromPC = 0;
+boolean newData = false;
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+void parseData() {      // split the data into its parts
+
+    char * strtokIndx; // this is used by strtok() as an index
+    strtokIndx = strtok(tempChars, ",");
+    a = atof(strtokIndx); 
+
+    strtokIndx = strtok(NULL, ",");
+    b = atof(strtokIndx);     // convert this part to a float
+
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  scale.begin(DOUT, SCK_OUT);
+
+//   Serial.println("HX711 starting to read idle value");
+//   delay(1000);
+//   reading_Start = scale.read();
+//   Serial.print("Value: ");
+//   Serial.println(reading_Start);
+
+//   Serial.println("Initialize done, starting to read mass");
+//   delay(1000);
+}
+
+void loop() {
+
+  if (scale.is_ready()) {
+      
+    current_Reading = scale.read();// * 0.00167;
+    float mass = float(current_Reading) * 0.00167;/// 100000;
+    mass = mass - 1700;
+    Serial.print("actual reading: ");
+    Serial.print(current_Reading);
+    Serial.print("    |   ");
+    Serial.print("mass reading: ");
+    Serial.println(mass);
+  } else {
+    Serial.println("HX711 not found.");
+  }
+  
+  current_Reading = scale.read();
+  
+  delay(100);
+  
+}
--- RTU/fix_RTU04/rtu04/src/main.cpp
+++ RTU/fix_RTU04/rtu04/src/main.cpp
@@ -1,89 +1,26 @@
-#include <Arduino.h>
-#include <Wire.h>             //http://arduino.cc/en/Reference/Wire
-#include <HX711.h>
+#include "HX711.h"
 
+// HX711 circuit wiring
 #define SCK_OUT A2
 #define DOUT A1
-HX711  scale;
-/* This program takes 10 samples from LC + HX711B at
-   1-sec interval and then computes the average.*/
 
-unsigned long x = 0, y = 0; //x is gauge factor, is the ratio between the change
-                            // in resistance (ΔR / R) to the Strain value
-float res = 0;
-unsigned long dataArray[10];
-int j = 0;
-void setup()
-{
+HX711 scale;
+
+void setup() {
   Serial.begin(9600);
-  // pinMode(DOUT, INPUT); //data line  //Yellow cable
-  // pinMode(SCK_OUT, OUTPUT);  //SCK line  //Orange cable
   scale.begin(DOUT, SCK_OUT);
 }
 
+void loop() {
 
-float deltaL = 1.2;
-float L = 1.2;
-
-
-void clk()
-{
-  digitalWrite(SCK_OUT, HIGH);
-  digitalWrite(SCK_OUT, LOW);
-}
-
-long value_gain, value_offset = 0;
-long result = 0;
-
-float a_ref = 0, b_ref = 0, res_y1 = 0, res_y2 = 0;
-float x1, y1, x2,y2;
-
-void calc_calib_idle(float a, float b){
-  return b
-}
-
-void loop(){
-
-    // digitalWrite(A0, LOW);//SCK is made LL
-    // // while (digitalRead(DOUT) != LOW){ //wait until Data Line goes LOW
-    //   for (int i = 0; i < 24; i++){  //read 24-bit data from HX711
-      
-    //     clk();      //generate CLK pulse to get MSB-it at DOUT-pin
-    //     bitWrite(x, 0, digitalRead(DOUT));
-    //     x = x << 1;
-    //   }
-    //   clk();  //25th pulse
-    //   res = x * 0.000001;
-    //   Serial.println(x/120);
-    //   y = x;
-    //   x = 0;
-    //   delay(100);
-    // // }
-    // dataArray[j] = y;
-  
-  // 'Firmware yg Wian buat harus bisa dimasukkan parameter oleh penguji (
-  // calibration factor: nilai_gain dan nilai_offset, 
-  // agar didapat nilai_akhir = (nilai_baca_sensor * nilai_gain) + nilai_offset). 
-  // Kita butuhkan calibration factor tadi. 
-  // Jadi ada 2 strain gage yg dikirim, 1 utk reference, 1 utk dimasukkan calibration factor.'
-
+  if (scale.is_ready()) {
     long reading = scale.read();
-    // Serial.println(reading);
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
+  }
 
-    value_gain = Serial.read();
-    value_offset = Serial.read();
-
-    result = (reading * value_gain) + value_offset;
-    Serial.println(result);
-    // delay(100);
-  // Serial.println("===averaging process=========");
-  // unsigned long sum = 0;
-
-  // for (j = 0; j < 10; j++)
-  // {
-  //   sum += dataArray[j];
-  // }
-  // Serial.print("Average Count = ");
-  // sum = sum / 10;
-  // Serial.println(sum);
+  delay(1000);
+  
 }
